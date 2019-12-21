527724bfbedb7f563cddc11d1af276dd5c9dfcf7
Mauro Insacco <mauro.insacco@gmail.com>
2019-12-21 14:03:06 +0100

    added not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { NotFoundComponent } from "./shared/components/not-found/not-found.component";
 
 const routes: Routes = [
   {
     path: "",
     loadChildren: () => import("./pages/pages.module").then(m => m.PagesModule)
+  },
+  {
+    path: "page-not-found",
+    component: NotFoundComponent
+  },
+  {
+    path: "**",
+    redirectTo: "page-not-found"
   }
 ];
 
